ebb14d408d9159c7bb1959e2455c2ad5d9480189
Alan Gutierrez <alan@blogometer.com>
2008-05-22 03:02:33 +0000

    - Checking in new Swag CSV Maven project in Swag.
    git-svn-id: http://blogometer.com/svn/open/trunk/swag/csv@2052 3df9ba58-24f1-0310-ade5-ba95bd924a04

---
+++ src/main/java/com/agtrz/swag/csv/CSV.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright The Engine Room, LLC 2005. All Right Reserved.
+ */
+package com.agtrz.swag.csv;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Alan Gutierrez
+ */
+public class CSV {
+    private final static int UNQUOTED = 1;
+    
+    private final static int QUOTED = 2;
+    
+    private final static int FIRST_CHARACTER = 3;
+    
+    private final static int FIRST_FIELD = 4;
+    
+    private final Consumer _consumer;
+    
+    public interface Consumer
+    {
+        public void line(List listOfFields);
+    }
+    
+    public CSV(Consumer consumer)
+    {
+        _consumer = consumer;
+    }
+    
+    public void read(Reader reader)
+    throws IOException
+    {
+        ArrayList listOfFields = new ArrayList();
+        while (readLine(reader, listOfFields))
+        {
+            _consumer.line(listOfFields);
+            listOfFields.clear();
+        }
+    }
+    
+    
+    public static List readLine(Reader reader) throws IOException
+    {
+        ArrayList listOfFields = new ArrayList();
+        if (readLine(reader, listOfFields))
+        {
+            return listOfFields;
+        }
+        return null;
+    }
+    
+    /**
+     * Accomodates a missing newline at end of file.
+     *
+     * @param reader A reader for the CSV file.
+     * @param lastChararacter The last character read.
+     *
+     * @return A newline if at eof and the last character was not a newline, otherwise
+     *         the result of read.
+     *
+     * @throws IOException For any I/O error.
+     */
+    private static int nextCharacter(Reader reader, char lastChararacter)
+    throws IOException
+    {
+        int character = reader.read();
+        if (character == -1
+            && !(lastChararacter == '\n' || lastChararacter == (char) 0))
+        {
+            return '\n';
+        }
+        return character;
+    }
+    
+    /**
+     * @todo Breaks where there is no newline after the last character.
+     */
+    private static boolean readLine(Reader reader, List list)
+    throws IOException
+    {
+        StringBuffer field = new StringBuffer();
+        
+        boolean hasFields = false;
+        boolean firstQuote = false;
+        int mode = FIRST_FIELD;
+        int character = 0;
+        char ch = (char) character;
+        while (ch != '\n' && (character = nextCharacter(reader, ch)) != -1)
+        {
+            ch = (char) character;
+            switch (mode)
+            {
+            case FIRST_FIELD:
+                hasFields = true;
+            case FIRST_CHARACTER:
+                switch (ch)
+                {
+                case '"':
+                    mode = QUOTED;
+                    break;
+                case '\r':
+                    break;
+                case '\n':
+                case ',':
+                    list.add(null);
+                    break;
+                default:
+                    mode = UNQUOTED;
+                    field.append(ch);
+                    break;
+                }
+                break;
+            case QUOTED:
+                switch (ch)
+                {
+                case '"':
+                    if (firstQuote)
+                    {
+                        firstQuote = false;
+                        field.append(ch);
+                    }
+                    else
+                    {
+                        firstQuote = true;
+                    }
+                    break;
+                case '\r':
+                    break;
+                case '\n':
+                case ',':
+                    if (firstQuote)
+                    {
+                        firstQuote = false;
+                        list.add(field.toString());
+                        field.setLength(0);
+                        mode = FIRST_CHARACTER;
+                    }
+                    else
+                    {
+                        field.append(ch);
+                    }
+                    break;
+                default:
+                    field.append(ch);
+                }
+                break;
+            case UNQUOTED:
+                switch (ch)
+                {
+                case '\r':
+                    break;
+                case ',':
+                case '\n':
+                    list.add(field.toString());
+                    field.setLength(0);
+                    mode = FIRST_CHARACTER;
+                    break;
+                default:
+                    field.append(ch);
+                    break;
+                }
+                break;
+            default:
+                throw new IllegalStateException();
+            }
+        }
+        
+        return hasFields;
+    }
+    
+    public final static String line(List listOfStrings)
+    {
+        StringBuffer line = new StringBuffer();
+        
+        String separator = "";
+        Iterator strings = listOfStrings.iterator();
+        while (strings.hasNext())
+        {
+            String string = strings.next().toString();
+            line.append(separator);
+            if (string.indexOf('"') != -1 || string.indexOf("\n") != -1)
+            {
+                line.append('"');
+                for (int i = 0; i < string.length(); i++)
+                {
+                    char ch = string.charAt(i);
+                    if (ch != '\r')
+                    {
+                        if (ch == '"')
+                        {
+                            line.append(ch);
+                        }
+                        else if (ch == '\n')
+                        {
+                            line.append('\r');
+                        }
+                        line.append(ch);
+                    }
+                }
+                line.append('"');
+            }
+            else
+            {
+                line.append(string);
+            }
+            separator = ",";
+        }
+        
+        line.append("\n");
+
+        return line.toString();
+    }
+}
+
+/* vim: set et sw=4 ts=4 ai tw=78 nowrap: */
---
+++ src/test/java/com/agtrz/swag/csv/CSVTestCase.java
@@ -0,0 +1,80 @@
+/* Copyright Alan Gutierrez 2006 */
+package com.agtrz.swag.csv;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+public class CSVTestCase
+extends TestCase
+{
+    private final static class ToArray
+    implements CSV.Consumer
+    {
+        public final List listOfRecords = new ArrayList();
+        
+        public void line(List listOfFields)
+        {
+            listOfRecords.add(listOfFields.toArray(new String[listOfFields.size()]));
+        }
+    }
+
+    public void testOneLine()
+    throws IOException
+    {
+        ToArray toArray = new ToArray();
+        String csv = "Hello,World!";
+        new CSV(toArray).read(new StringReader(csv));
+        assertEquals(1, toArray.listOfRecords.size());
+        String[][] records = (String[][]) toArray.listOfRecords.toArray(new String[1][]);
+        assertEquals("Hello", records[0][0]);
+        assertEquals("World!", records[0][1]);
+    }
+
+    public void testTwoLines()
+    throws IOException
+    {
+        ToArray toArray = new ToArray();
+        String csv = "Hello,World!\nOne,Two,Three";
+        new CSV(toArray).read(new StringReader(csv));
+        assertEquals(2, toArray.listOfRecords.size());
+        String[][] records = (String[][]) toArray.listOfRecords.toArray(new String[2][]);
+        assertEquals("Hello", records[0][0]);
+        assertEquals("World!", records[0][1]);
+        assertEquals("One", records[1][0]);
+        assertEquals("Two", records[1][1]);
+        assertEquals("Three", records[1][2]);
+    }
+
+    public void testCommaAtEndOfLine()
+    throws IOException
+    {
+        ToArray toArray = new ToArray();
+        String csv = "Barbara_Foundas,Algeirs_Council_of_Neighborhood_Presidents,representative,4,,,,,,,,,,,,,,,,\nOne,Two,Three";
+        new CSV(toArray).read(new StringReader(csv));
+        assertEquals(2, toArray.listOfRecords.size());
+        String[][] records = (String[][]) toArray.listOfRecords.toArray(new String[2][]);
+        assertEquals("Barbara_Foundas", records[0][0]);
+        assertEquals("Algeirs_Council_of_Neighborhood_Presidents", records[0][1]);
+        assertEquals("One", records[1][0]);
+        assertEquals("Two", records[1][1]);
+        assertEquals("Three", records[1][2]);
+    }
+    
+    public void testLine()
+    {
+        List listOfStrings = new ArrayList();
+        
+        listOfStrings.add("Hello");
+        listOfStrings.add("I said, \"Parsley.\"");
+        listOfStrings.add("I don't like to complain\nbut...");
+        listOfStrings.add("C:\\WINNT");
+        
+        System.out.print(CSV.line(listOfStrings));
+    }
+}
+
+/* vim: set et sw=4 ts=4 ai tw=78 nowrap: */
