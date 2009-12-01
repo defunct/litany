208b9132a19e4d44660523b3a9bdda1693f15b76
Alan Gutierrez <alan@blogometer.com>
2009-12-01 12:05:01 -0600

    Updating Mix build.

---
+++ src/mix/java/com/goodworkalan/litany/mix/LitanyProject.java
@@ -0,0 +1,22 @@
+package com.goodworkalan.litany.mix;
+
+import com.goodworkalan.go.go.Artifact;
+import com.goodworkalan.mix.ProjectModule;
+import com.goodworkalan.mix.builder.Builder;
+import com.goodworkalan.mix.builder.JavaProject;
+
+public class LitanyProject extends ProjectModule {
+    @Override
+    public void build(Builder builder) {
+        builder
+            .cookbook(JavaProject.class)
+                .produces(new Artifact("com.goodworkalan", "litany", "0.7"))
+                .test()
+                    .depends()
+                        .artifact(new Artifact("org.testng/testng/5.10"))
+                        .end()
+                    .end()
+                .end()
+            .end();
+    }
+}
