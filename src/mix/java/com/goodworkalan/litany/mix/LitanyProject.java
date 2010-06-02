feb3df4ed8927b7f1a53f8d5ac94636dcf66c41f
Alan Gutierrez <alan@blogometer.com>
2010-06-02 14:03:22 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/litany/mix/LitanyProject.java
+++ src/mix/java/com/goodworkalan/litany/mix/LitanyProject.java
@@ -4,16 +4,24 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class LitanyProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Litany.
+ *
+ * @author Alan Gutierrez
+ */
+public class LitanyProject implements ProjectModule {
+    /**
+     * Build the project definition for Litany.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.litany/litany/0.7")
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
